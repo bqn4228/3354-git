981e6dba1de7b8ab3ec1b2c469149da0926c7d08
BAO NGUYEN <BAO NGUYEN>
2017-01-31 23:45:35 -0600

    Modified Adder program error message

--- Adder/src/adder/Main.java
+++ Adder/src/adder/Main.java
@@ -6,12 +6,17 @@ public class Main {
         try {
             int result = addArguments(args);
             System.out.println(result);
-        } catch (Exception e) {
-            System.err.println("Please provide three integers to add");
-        }
+        } catch(InputMismatchException a){
+		System.out.println("Invalid input");
+	catch (Exception e) {
+            System.err.println("Please provide three values to add");
+        } 
     }
 
     private static int addArguments(String[] args) {
+if(args[0] == "-")
+	return -Integer.valueOf(args[1]) - Integer.valueOf(args[2]);
+else
         return Integer.valueOf(args[0]) + Integer.valueOf(args[1]) + Integer.valueOf(args[2]);
     }
 }
